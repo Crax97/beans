f12bd65df435ef4bb91346a2b46386e1599e907d
Crax <gsolimeno97@gmail.com>
2019-09-17 12:37:52 +0200

    Added multiple envs and testing

--- src/beans/mod.rs
+++ src/beans/mod.rs
@@ -18,14 +18,65 @@ mod tests {
             Value::Num(d.sin())
         };
 
-        let glob = beans::create_env();
+        let glob = beans::create_global();
         glob.as_ref().borrow_mut().bind_fun("sin", sin_v, 1);
 
         let expr = "sin(0.0);";
-        let mut evaluator = beans::make_evaluator(glob);
+        let mut evaluator = beans::create_evaluator(glob);
         match beans::exec_string(expr, &mut evaluator) {
             StatementResult::Ok(v) => assert!(v.as_numeric() == 0.0),
             _ => panic!("Failed evaluating sin(0)"),
+        }
+    }
+
+    #[test]
+    fn two_envs() {
+        let glob = beans::create_global();
+
+        let mut first = beans::create_with_global(glob.clone());
+        let mut sec = beans::create_with_global(glob.clone());
+
+        let cos_fun = |vs: Vec<Value>| {
+            let d = vs.first().unwrap().as_numeric();
+            Value::Num(d.cos())
+        };
+
+        let sin_fun = |vs: Vec<Value>| {
+            let d = vs.first().unwrap().as_numeric();
+            Value::Num(d.sin())
+        };
+
+        first.as_ref().borrow_mut().bind_fun("cos", cos_fun, 1);
+        sec.as_ref().borrow_mut().bind_fun("sin", sin_fun, 1);
+
+        let sin_expr = "sin(0.0);";
+        let cos_expr = "cos(0.0);";
+
+        let mut evaluator1 = beans::create_evaluator(first);
+        let mut evaluator2 = beans::create_evaluator(sec);
+
+        match beans::exec_string(sin_expr, &mut evaluator2) {
+            StatementResult::Ok(v) => assert!(v.as_numeric() == 0.0),
+            _ => panic!("Failed evaluating sin(0)"),
+        }
+        match beans::exec_string(cos_expr, &mut evaluator1) {
+            StatementResult::Ok(v) => assert!(v.as_numeric() == 1.0),
+            _ => panic!("Failed evaluating cos(0)"),
+        }
+
+        match beans::exec_string(sin_expr, &mut evaluator1) {
+            StatementResult::Ok(v) => match v {
+                Value::Nil => {}
+                _ => unreachable!(),
+            },
+            _ => {}
+        }
+        match beans::exec_string(cos_expr, &mut evaluator2) {
+            StatementResult::Ok(v) => match v {
+                Value::Nil => {}
+                _ => unreachable!(),
+            },
+            _ => {}
         }
     }
 }
@@ -45,12 +96,16 @@ pub mod beans {
         env: Rc<RefCell<Env>>,
     }
 
-    pub fn create_env() -> Rc<RefCell<Env>> {
+    pub fn create_global() -> Rc<RefCell<Env>> {
         Rc::new(RefCell::new(Env::new()))
     }
 
-    pub fn make_evaluator(global: Rc<RefCell<Env>>) -> Evaluator {
-        Evaluator::new_with_global(global)
+    pub fn create_with_global(global: Rc<RefCell<Env>>) -> Rc<RefCell<Env>> {
+        Rc::new(RefCell::new(Env::new_enclosing(global)))
+    }
+
+    pub fn create_evaluator(env: Rc<RefCell<Env>>) -> Evaluator {
+        Evaluator::new_with_global(env)
     }
 
     pub fn exec_string(program: &str, evaluator: &mut Evaluator) -> StatementResult {
